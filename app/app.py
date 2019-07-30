a1a00dab2a5cbbfd4d5a0c92d962adfddf9a06d5
shind07 <shind@sfu.ca>
2019-07-30 05:11:08 -0700

    try connecting to RDS

--- app/app.py
+++ app/app.py
@@ -1,4 +1,17 @@
+from dotenv import load_dotenv
+load_dotenv()
 
+import os
+from sqlalchemy import create_engine
 
 if __name__ == "__main__":
-    print("in my app")
+    #print(os.environ)
+    connection_string = 'postgresql://{}:{}@{}'.format(
+        os.getenv('RDS_USER'),
+        os.getenv('RDS_PASSWORD'),
+        os.getenv('RDS_HOSTNAME')
+    )
+    print(connection_string)
+    with create_engine(connection_string).connect() as conn:
+        print(conn.table_names())
+
